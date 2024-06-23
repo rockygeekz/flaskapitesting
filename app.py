528f42247d6b1dc50ea68699cd3c05ae210d87c5
Rakesh s <rakesh.s552004@gmail.com>
2024-06-23 18:25:48 +0530

    major changes changed only arrow

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from textblob import TextBlob
+# from transformers import pipeline  # Uncomment if using transformers
+
+app = Flask(__name__)
+
+@app.route('/', methods=['get'])
+def hello_func():
+    return "HEY your api is working"
+
+
+@app.route('/analyze', methods=['POST'])
+def analyze_sentiment():
+    data = request.json
+    text = data.get('text')
+    
+    # Using TextBlob
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+    sentiment_label = 'positive' if sentiment > 0 else 'negative' if sentiment < 0 else 'neutral'
+
+    # Using Hugging Face transformers
+    # classifier = pipeline('sentiment-analysis')
+    # result = classifier(text)
+    # sentiment_label = result[0]['label']
+
+    return jsonify({'sentiment': sentiment_label})
+
+if __name__ == '__main__':
+    app.run(debug=True)
